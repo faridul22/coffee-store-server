1350fd61562e0c0388c4ef90ab12704c93b96b66
faridul22 <96929890+faridul22@users.noreply.github.com>
2024-08-26 12:26:31 +0600

    put methods added

--- index.js
+++ index.js
@@ -32,13 +32,38 @@ async function run() {
             res.send(coffee)
         });
 
-
+        app.get('/coffee/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await coffeeCollection.findOne(query);
+            res.send(result)
+        })
 
         app.post('/coffee', async (req, res) => {
             const coffee = req.body;
             const result = await coffeeCollection.insertOne(coffee);
             res.send(result)
         });
+
+        app.put('/coffee/:id', async (req, res) => {
+            const id = req.params.id;
+            const coffee = req.body;
+            const filter = { _id: new ObjectId(id) };
+            const options = { upsert: true };
+            const updatedCoffee = {
+                $set: {
+                    name: coffee.name,
+                    quantity: coffee.quantity,
+                    supplier: coffee.supplier,
+                    taste: coffee.taste,
+                    category: coffee.category,
+                    details: coffee.details,
+                    photo: coffee.photo,
+                }
+            }
+            const result = await coffeeCollection.updateOne(filter, updatedCoffee, options)
+            res.send(result)
+        })
 
         app.delete('/coffee/:id', async (req, res) => {
             const id = req.params.id;
